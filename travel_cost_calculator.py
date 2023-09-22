eb86a71469d4aa42f628039db3bd9144379f2c93
ravishaaaan <139684795+ravishaaaan@users.noreply.github.com>
2023-09-22 10:54:28 +0000

    EG_2020_4277

--- travel_cost_calculator.py
+++ travel_cost_calculator.py
@@ -1,49 +1,74 @@
+#4277 Welikumbura R.W.R.L.
+
 from csv import *
 
-a = {}
-b = {}
-c = {}
+hotel_rates = {}     # Dictionary for hotel rates
+exchange_rates = {}  # Dictionary for exchange rates
+flight_costs = {}    # Dictionary for flight costs
 
-def lhr(file):  
-    with open(file) as h:
-        r = reader(h)
-        for row in r:
-            a[row[0]] = float(row[1])
+# Function to load hotel rates from a CSV file
+def load_hotel_rates(file):
+    with open(file) as hotel_file:
+        csv_reader = reader(hotel_file)
+        for row in csv_reader:
+            hotel_rates[row[0]] = float(row[1])
 
-def ler(file): 
-    with open(file) as e:
-        r = reader(e)
-        for row in r:
-            b[row[0].upper()] = float(row[1]) * 1 
+# Function to load exchange rates from a CSV file
+def load_exchange_rates(file):
+    with open(file) as exchange_file:
+        csv_reader = reader(exchange_file)
+        for row in csv_reader:
+            # Store exchange rates as uppercase currency codes
+            exchange_rates[row[0].upper()] = float(row[1])
 
-def lfr(file):
-    with open(file) as f:
-        r = reader(f)
-        for row in r:
-            c[row[0]] = float(row[1])
+# Function to load flight costs from a CSV file
+def load_flight_costs(file):
+    with open(file) as flight_file:
+        csv_reader = reader(flight_file)
+        for row in csv_reader:
+            flight_costs[row[0]] = float(row[1])
 
+# Main function to calculate and display total travel cost
 def main():
-    lhr('data/hotel_rates.csv')
-    ler('data/exchange_rates.csv')
-    lfr('data/flight_costs.csv')
 
-    d = input("Enter your destination: ").upper()
 
-    f = c.get(d, 0.0)
-    h = a.get(d, 0.0)
+# Load data from CSV files
+    load_hotel_rates('data/hotel_rates.csv')
+    load_exchange_rates('data/exchange_rates.csv')
+    load_flight_costs('data/flight_costs.csv')
 
-    days = int(input("Enter your stay duration in days: "))
-    h *= days
-    total = f + h
 
-    print(f"Flight cost: USD {f:.2f}")
-    print(f"Hotel cost for {days} days: USD {h:.2f}")
-    print(f"Total: USD {total:.2f}")
+# Get the destination from the user and convert it to uppercase
+    destination = input("Enter your destination: ").upper()
 
-    currency = input(f"Select your currency for final price estimation ({', '.join(b.keys())}): ")
 
-    p = total * b[currency]
-    print(f"Total in {currency}: {p:.2f}")
+# Get flight cost and hotel rate with destination
+    flight_cost = flight_costs.get(destination, 0.0)
+    hotel_rate = hotel_rates.get(destination, 0.0)
+
+
+# Get the duration of stay from the user and calculate hotel cost
+    stay_duration = int(input("Enter your stay duration in days: "))
+    hotel_cost = hotel_rate * stay_duration
+
+# Calculate the total cost
+    total_cost = flight_cost + hotel_cost
+
+
+
+
+# Display flight cost, hotel cost, and total cost
+    print(f"Flight cost: USD {flight_cost:.2f}")
+    print(f"Hotel cost for {stay_duration} days: USD {hotel_cost:.2f}")
+    print(f"Total: USD {total_cost:.2f}")
+
+# Get the user's preferred currency for final price estimation
+    available_currencies = ', '.join(exchange_rates.keys())
+    selected_currency = input(f"Select your currency for final price estimation ({available_currencies}): ")
+
+ # Calculate the total cost selected currency
+    price_in_selected_currency = total_cost * exchange_rates.get(selected_currency, 1.0)
+    print(f"Total in {selected_currency}: {price_in_selected_currency:.2f}")
 
 if __name__ == "__main__":
     main()
